1723af8de7fbba92518392eda9974492743ab0bb
Daniel Carazo <danielcarazo20@gmail.com>
2021-08-04 09:53:57 -0400

    linked database into server file

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const mysql = require('mysql2');
+
 const express = require ('express');
 
 const PORT = process.env.PORT || 3001;
@@ -6,6 +8,23 @@ const app = express ();
 // Express middleware 
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+// Connect to Database 
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MYSQL username,
+        user: 'root',
+        // MYSQL password
+        password: 'Password',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 //Default response for any other request (Not Found)
 app.use((req, res) => {
