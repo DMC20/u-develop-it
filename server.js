95cfd74ef5399dad37b975ff4d05eaa0aa7b155d
Daniel Carazo <danielcarazo20@gmail.com>
2021-08-04 10:14:09 -0400

    uncommented code

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
-// db.query(`SELECT * FROM candidates`, (err, rows) => {
-//     console.log(rows);
-// });
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // Create a candidate 
 const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
@@ -38,21 +38,21 @@ db.query(sql, params, (err, result) => {
     console.log(result);
 })
 
-// // GET a single candidate
-// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(row)
-// });
+// GET a single candidate
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(row)
+});
 
-// // Delete a candidate 
-// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err,result) => {
-//     if (err) {
-//         console.log(err)
-//     }
-//     console.log(result)
-// })
+// Delete a candidate 
+db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err,result) => {
+    if (err) {
+        console.log(err)
+    }
+    console.log(result)
+})
 
 //Default response for any other request (Not Found)
 app.use((req, res) => {
